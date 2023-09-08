0085bc6380f348ab8c5d5f7fdb546150920fcf8c
Your Name <you@example.com>
2023-09-08 22:32:17 +0530

    first commit

---
+++ build/precache-manifest.51f4fc6d05126001879aa4bf5446d787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae170c61d71b5565945abd14b22df6f",
+    "url": "/Micro_Mouse/index.html"
+  },
+  {
+    "revision": "db62a7a465cabf356173",
+    "url": "/Micro_Mouse/static/css/main.4064710e.chunk.css"
+  },
+  {
+    "revision": "3f51ea05862ffd4db38d",
+    "url": "/Micro_Mouse/static/js/2.f0583ba6.chunk.js"
+  },
+  {
+    "revision": "db62a7a465cabf356173",
+    "url": "/Micro_Mouse/static/js/main.0f116b66.chunk.js"
+  },
+  {
+    "revision": "6d0b3a7efd9fe5f4dee5",
+    "url": "/Micro_Mouse/static/js/runtime-main.5fa574c5.js"
+  }
+]);
